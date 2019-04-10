fb5c40ba9793b80e29412ac45c9774583030ad8a
Kowrog77 <43824900+Kowrog77@users.noreply.github.com>
2019-04-10 15:57:18 -0500

    4-10-2019

--- serverjsStuff/index.js
+++ serverjsStuff/index.js
@@ -1,5 +1,51 @@
 var http = require("http");
-
+var url = require("url");
+var fs = require("fs");
 http.createServer(function(req, res){
-    res.end("hello Joe");
-})
+    // res.writeHead('200',{
+    //     "content-type": "text/plain"
+    // })
+    // res.end("hello Joe");
+    // res.writeHead('307',{
+    //     "Location": "http://www.w3schools.com"
+    // })
+    // res.end("hello Joe");
+    var pathname = url.parse(req.url).pathname;
+    console.log(pathname);
+    // if(pathname ==="/"){
+    //     res.writeHead('200',{
+    //         "content-type":"text/plain"
+    //     });
+    //     res.end("index.html");
+    // }else if (pathname === '/about.html'){
+    //     res.writeHead('200' ,{
+    //         "content-type":"text/plain"
+    //     });
+    //     res.end("about.html")
+    // }else{
+    //     res.writeHead('400' ,{
+    //         "content-type":"text/plain"
+    //     });
+    //         res.end("page Not found")
+    // }
+    var theFile;
+    
+    switch (pathname) {
+        case'/about':
+        case '/about.html':
+            theFile ="about.html"
+            break;
+    
+        default:
+            theFile ="index.html"
+            break;
+    }
+    fs.readFile(theFile, function(err,data){
+        res.writeHead('200',{
+            "content-type": "text/html"
+        })
+        res.end(data);
+    })
+}).listen(3002);
+console.log("Server Running")
+
